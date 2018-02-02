66817e2e7ed1a84186629c2609c00a8f8d5680c1
Ryan Zimmerman <opensrc@ryanzim.com>
2018-02-02 16:36:39 -0500

    Stub unneeded modules

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,9 @@ import commonjs from 'rollup-plugin-commonjs';
 import builtins from 'rollup-plugin-node-builtins';
 import globals from 'rollup-plugin-node-globals';
 
+import postcssPkg from 'postcss/package.json'
+const stubs = Object.keys(postcssPkg.browser)
+
 export default {
   input: 'src/main.js',
   plugins: [
@@ -13,8 +16,13 @@ export default {
     builtins(),
     globals(),
   ],
+  external: stubs,
   output: {
     file: 'bundle.js',
     format: 'iife',
+    globals: stubs.reduce((obj, mod) => {
+      obj[mod] = 'window'
+      return obj
+    }, {}),
   },
 };
